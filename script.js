b2e3ad58556793c31af734a4120182c9c7f51394
Paul <palu.maynard@gmail.com>
2016-10-10 20:46:40 -0400

    Basic code

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+let nodes = [
+	0.70,
+	0.74,
+	1.20,
+	1.24,
+	1.70,
+	1.74,
+	2.20,
+	2.24,
+	2.70,
+	2.74,
+	3.20,
+	3.24
+].map(n => n - .22)
+
 let update = () => {
-    console.log('derp')
-    return true
+    let s = +size.value / 1.25
+    let t = +node.value
+    let out = `	MODEL
+	{
+		model = Squad/Parts/Aero/fairings/AutoTruss
+		scale = ${s},${s},${s}
+		position = 0.0,${t},0.0
+	}`
+	let n = nodes.map(n => n + t)
+    cfg.innerHTML = out
 }
 
-size.addEventListener('input', update)
-top.addEventListener('input', update)
+calc.addEventListener('click', update)
